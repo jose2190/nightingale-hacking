00b7ba7e8800de4fdf5bd5e631cbdad7236a2419
Matt Parnell <parwok@gmail.com>
2011-09-15 04:05:12 -0500

    add git ...we'll remove the svn dirs maybe, but they're slightly necessary to grab svn crap too

---
+++ dependencies/linux-i686/mozilla-1.9.2/debug/include/nspluginroot.h
@@ -0,0 +1,12 @@
+/*
+ * DO NOT EDIT.  THIS FILE IS GENERATED FROM /builds/vendor/sb_linbot03_vendor_trunk/build/vendor/xulrunner-1.9.2/mozilla/modules/plugin/base/public/nspluginroot.idl
+ */
+
+#ifndef __gen_nspluginroot_h__
+#define __gen_nspluginroot_h__
+/* For IDL files that don't want to include root IDL files. */
+#ifndef NS_NO_VTABLE
+#define NS_NO_VTABLE
+#endif
+
+#endif /* __gen_nspluginroot_h__ */
